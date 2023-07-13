7711c322af40defa1ad7e6d7028513def270e4da
ROUX <thomas.roux94@hotmail.fr>
2023-07-13 11:46:03 +0200

    fermer la modale #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,10 +1,10 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
@@ -17,7 +17,14 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
-
-
+// close modal
+// on récupère l'élement close
+const closeBtn = document.getElementsByClassName("close")[0];
+// on ajoute un event listener sur le click et on effectue l'action close modal
+closeBtn.addEventListener("click", closeModal);
+// passe le modal en display none 
+function closeModal() {
+    modalbg.style.display = "none";
+};
